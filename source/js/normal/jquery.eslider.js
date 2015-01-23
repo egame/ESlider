a1d45411ead8ecce7e40137b6e621a204f991c02
zhengchen <zhengchen0306@163.com>
2015-01-23 17:43:51 +0800

    简化html初始结构，功能性html代码通过js动态生成

--- source/js/normal/jquery.eslider.js
+++ source/js/normal/jquery.eslider.js
@@ -13,172 +13,14 @@
     });
     $.fn.eslider = function(options) {    
         this.each(function() {
-            var opts = $.extend({}, $.fn.eslider.defaults, options);    
-            var Jcon = $(this),
-            //图片索引状态按钮
-            Jtabs = Jcon.find(opts.tabBox).find(opts.tabItem),
-            //图片显示框
-            JconBox = Jcon.find(opts.conBox),
-            //图片滚动框
-            JconInner = $('<div style="position:absolute;"></div>'),
-            //图片列表
-            Jcons = JconBox.find(opts.conItem),
-            //上一页
-            JpreBtn = Jcon.find(opts.btnPre),
-            //下一页
-            JnextBtn = Jcon.find(opts.btnNext),
-            //len:图片个数
-            //nlen:克隆首尾图片后个数，用于循环展示
-            //curI当前索引值
-            //unitW、unitH显示框宽高
-            //delayTime 延迟触发时间
-            //isMoving  是否在动画中
-            len = Jcons.length, nlen = len + 2, curI = 0, unitW = JconBox.width(), unitH = JconBox.height();
-            var go2con = null, timer = null, delayTime = null, isMoving = false;
-
-            //初始化dom操作
-            JconInner.append(Jcons);
-            if (opts.type == "h" || opts.type == "v") {
-                JconInner.append(Jcons.eq(0).clone());
-                JconInner.prepend(Jcons.eq(len-1).clone());
-            }
-            JconBox.append(JconInner);
-            Jcons = JconInner.find(opts.conItem);
-
-            Jcons.css({width: unitW + "px", height: unitH + "px", overflow: "hidden"});
-            if (opts.type == "h") {
-                JconInner.css({left:-unitW + "px", width: nlen * unitW + "px", height: unitH + "px"});
-            }else if (opts.type == "v") {
-                JconInner.css({top:-unitH + "px"});
-            }else if(opts.type == "a"){
-                Jcons.css({position: "absolute"}).hide().eq(curI).show();
-            }
-
-            function go2next () {
-                var i = curI + 1;
-                if (i >= len) {
-                    i = 0;
-                    if (opts.type == "h") {
-                        JconInner.css({left: 0});
-                    }else if (opts.type == "v") {
-                        JconInner.css({top: 0});
-                    }
-                }
-                go2con(i);
-            }
-            function go2pre () {
-                var i = curI - 1;
-                if (i < 0) {
-                    i = len - 1;
-                    if (opts.type == "h") {
-                        JconInner.css({left: -(len+1)*unitW});
-                    }else if (opts.type == "v") {
-                        JconInner.css({top: -(len+1)*unitH});
-                    }
-                }
-                go2con(i);
-            }
-
-            go2con = function(index){
-                switch(opts.type) {
-                    case 'h':
-                        isMoving = true;
-                        var l = - (index * unitW) - unitW;
-                        JconInner.stop().animate({left: l + "px"}, opts.speed, opts.easing, function() {
-                            isMoving = false;
-                        });
-                    break;
-                    case 'v':
-                        isMoving = true;
-                        var l = - (index * unitH) - unitH;
-                        JconInner.stop().animate({top: l + "px"}, opts.speed, opts.easing, function() {
-                            isMoving = false;
-                        });
-                    break;
-                    case 'a':
-                        if (curI == index) {
-                            return;
-                        }
-                        Jcons.stop().hide().eq(index).fadeIn(opts.speed);
-                        Jcons.eq(curI).show().fadeOut(opts.speed);
-                    break;
-                    case 'n':
-                        Jcons.hide().eq(index).show();
-                    break;
-                    default:
-                        isMoving = true;
-                        var l = - (index * unitW) - unitW;
-                        JconInner.stop().animate({left: l + "px"}, opts.speed, opts.easing, function() {
-                            isMoving = false;
-                        });
-                }
-                Jtabs.removeClass(opts.tabOnClass).eq(index).addClass(opts.tabOnClass);
-                curI = index;
-
-                if (!!opts.callback) {
-                    opts.callback(index, Jcon);
-                }
-            };
-
-            if (opts.ev == 'mouseover') {
-                //延迟触发
-                Jtabs.hover(function(){
-                    var i = $(this).index();
-                    clearTimeout(delayTime);
-                    delayTime = null;
-                    delayTime = setTimeout(function() {
-                        go2con(i);
-                    },opts.hoverDelay);
-                },function() {
-                    clearTimeout(delayTime);
-                    delayTime = null;
-                });
-            }else{
-                Jtabs.bind(opts.ev, function(){
-                    var Jme = $(this), i = Jme.index();
-                    go2con(i);
-                });
-            }
-
-            JpreBtn.click(function(){
-                if (!isMoving) {
-                    go2pre();
-                }
-            });
-            JnextBtn.click(function(){
-                if (!isMoving) {
-                    go2next();
-                }
-            });
-
-            if (opts.auto) {
-                Jcon.bind("mouseover", function(){
-                    clearInterval(timer);
-                    timer = null;
-                }).bind("mouseout", function(){
-                    clearInterval(timer);
-                    timer = null;
-                    timer = setInterval(function(){
-                        go2next();
-                    }, opts.time);
-                }).trigger("mouseout");
-            }
+            $.fn.eslider.init($(this), options);
         });
-
     };    
     // 默认参数
     $.fn.eslider.defaults = {    
-        //按钮包裹元素
-        tabBox: ".fn_tab_nav",
-        //按钮元素
-        tabItem: ".fn_tab_item",
-        //上一页
-        btnPre: ".fn_pre",
-        //下一页
-        btnNext: ".fn_next",
-        //内容包裹元素
+        //内容包裹元素选择器
         conBox: ".fn_con",
-        //内容元素
+        //内容元素选择器
         conItem: ".fn_con_item",
         //tab选中样式名
         tabOnClass: "on",
@@ -186,6 +28,10 @@
         type: "h",
         //触发事件
         ev: "mouseover",
+        //隐藏标题栏
+        hideTitleBar: false,
+        //隐藏前进\后退按钮
+        hideBtn: false,
         //延迟触发时长，事件为mouseover时启用，防止切换按钮误触
         hoverDelay: 50,
         //是否自动切换 true\false
@@ -199,8 +45,176 @@
         //跳转到对应图片后回调，返回当前索引值及顶级包裹元素对象
         callback: null
     };     
-    //这个只需要调用一次，且不一定要在ready块中调用  
-    $.fn.eslider.defaults.foreground = 'blue';    
+    $.fn.eslider.init = function(Jobj, options) {
+        var opts = $.extend({}, $.fn.eslider.defaults, options);    
+        var Jcon = Jobj,
+        //图片显示框
+        JconBox = Jcon.find(opts.conBox),
+        //图片滚动框
+        JconInner = $('<div style="position:absolute;"></div>'),
+        //图片列表
+        Jcons = JconBox.find(opts.conItem),
+        JpreBtn = null,
+        JnextBtn = null,
+        //len:图片个数
+        //nlen:克隆首尾图片后个数，用于循环展示
+        //curI当前索引值
+        //unitW、unitH显示框宽高
+        //delayTime 延迟触发时间
+        //isMoving  是否在动画中
+        len = Jcons.length, nlen = len + 2, curI = 0, unitW = JconBox.width(), unitH = JconBox.height();
+        var timer = null, delayTime = null, isMoving = false, titleArr = [];
 
+        Jcons.each(function() {
+            titleArr.push($(this).find('img').attr('title'));
+        });
+
+        function go2next () {
+            var i = curI + 1;
+            if (i >= len) {
+                i = 0;
+                if (opts.type == "h") {
+                    JconInner.css({left: 0});
+                }else if (opts.type == "v") {
+                    JconInner.css({top: 0});
+                }
+            }
+            go2con(i);
+        }
+        function go2pre () {
+            var i = curI - 1;
+            if (i < 0) {
+                i = len - 1;
+                if (opts.type == "h") {
+                    JconInner.css({left: -(len+1)*unitW});
+                }else if (opts.type == "v") {
+                    JconInner.css({top: -(len+1)*unitH});
+                }
+            }
+            go2con(i);
+        }
+        function go2con(index){
+            switch(opts.type) {
+                case 'h':
+                    isMoving = true;
+                var l = - (index * unitW) - unitW;
+                JconInner.stop().animate({left: l + "px"}, opts.speed, opts.easing, function() {
+                    isMoving = false;
+                });
+                break;
+                case 'v':
+                    isMoving = true;
+                var l = - (index * unitH) - unitH;
+                JconInner.stop().animate({top: l + "px"}, opts.speed, opts.easing, function() {
+                    isMoving = false;
+                });
+                break;
+                case 'a':
+                    if (curI == index) {
+                    return;
+                }
+                Jcons.stop().hide().eq(index).fadeIn(opts.speed);
+                Jcons.eq(curI).show().fadeOut(opts.speed);
+                break;
+                case 'n':
+                    Jcons.hide().eq(index).show();
+                break;
+                default:
+                    isMoving = true;
+                var l = - (index * unitW) - unitW;
+                JconInner.stop().animate({left: l + "px"}, opts.speed, opts.easing, function() {
+                    isMoving = false;
+                });
+            }
+
+            Jtab.find('.tab_item').removeClass(opts.tabOnClass).eq(index).addClass(opts.tabOnClass);
+            if (!opts.hideTitleBar) {
+                Jtitle.html(titleArr[index]);
+            }
+
+            curI = index;
+
+            if (!!opts.callback) {
+                opts.callback(index, Jcon);
+            }
+        };
+
+        //初始化dom操作
+        if (!opts.hideTitleBar) {
+            var Jtips = $('<div class="tips_con"><div class="tips_bg"></div></div>').appendTo(Jcon);
+            var Jtitle = $('<div class="tips_tt">'+titleArr[curI]+'</div>').appendTo(Jtips);
+        }
+        var Jtab = $('<ul class="tab_nav"></ul>').appendTo(Jcon);
+        for (var i = 0; i < len; i++) {
+            (function(_i) {
+                if (opts.ev == 'mouseover') {
+                    //延迟触发
+                    $('<li class="tab_item"></li>').hover(function() {
+                        clearTimeout(delayTime);
+                        delayTime = null;
+                        delayTime = setTimeout(function() {
+                            go2con(_i);
+                        },opts.hoverDelay);
+                    },function() {
+                        clearTimeout(delayTime);
+                        delayTime = null;
+                    }).appendTo(Jtab);
+                }else{
+                    $('<li class="tab_item"></li>').bind(opts.ev, function() {
+                        go2con(_i);
+                    }).appendTo(Jtab);
+                }
+            })(i);
+        }
+        Jtab.find('.tab_item').eq(curI).addClass(opts.tabOnClass);
+        if(!opts.hideBtn){
+            JpreBtn = $('<a href="javascript:;" class="btn_pre">上一张</a>').click(function(){
+                if (!isMoving) {
+                    go2pre();
+                }
+            }).hide().appendTo(Jcon);
+            JnextBtn = $('<a href="javascript:;" class="btn_next">下一张</a>').click(function(){
+                if (!isMoving) {
+                    go2next();
+                }
+            }).hide().appendTo(Jcon);
+            Jcon.hover(function() {
+                JpreBtn.show();
+                JnextBtn.show();
+            },function() {
+                JpreBtn.hide();
+                JnextBtn.hide();
+            });
+        }
+        JconInner.append(Jcons);
+        if (opts.type == "h" || opts.type == "v") {
+            JconInner.append(Jcons.eq(0).clone());
+            JconInner.prepend(Jcons.eq(len-1).clone());
+        }
+        JconBox.append(JconInner);
+        Jcons = JconInner.find(opts.conItem);
+        Jcons.css({width: unitW + "px", height: unitH + "px", overflow: "hidden"});
+        if (opts.type == "h") {
+            JconInner.css({left:-unitW + "px", width: nlen * unitW + "px", height: unitH + "px"});
+        }else if (opts.type == "v") {
+            JconInner.css({top:-unitH + "px"});
+        }else if(opts.type == "a"){
+            Jcons.css({position: "absolute"}).hide().eq(curI).show();
+        }
+
+        //自动轮播处理
+        if (opts.auto) {
+            Jcon.hover(function(){
+                clearInterval(timer);
+                timer = null;
+            }, function(){
+                clearInterval(timer);
+                timer = null;
+                timer = setInterval(function(){
+                    go2next();
+                }, opts.time);
+            }).trigger("mouseout");
+        }
+    };
 })(jQuery)
 
